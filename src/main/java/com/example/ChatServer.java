67769fcd84f33b07ee91eaa0edd2633ce90180c7
rafal0kiljan-dev <rafal.0.kiljan@gmail.com>
2025-09-15 00:03:09 +0200

    Update ChatServer.java

--- src/main/java/com/example/ChatServer.java
+++ src/main/java/com/example/ChatServer.java
@@ -16,7 +16,7 @@ public class ChatServer {
             server.start();
             System.out.println("--- server is running");
             System.out.println("--- press any key to stop the server");
-            BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+            var bufferRead = new BufferedReader(new InputStreamReader(System.in));
             bufferRead.readLine();
         } catch (Exception e) {
             e.printStackTrace();
